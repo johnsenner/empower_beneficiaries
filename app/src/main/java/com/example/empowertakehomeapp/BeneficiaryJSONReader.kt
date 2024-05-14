e16052cca5cb4998bc1d1a922a3597106de33c4c
John Senner <john.senner@gmail.com>
2024-05-14 00:44:48 -0600

    cleanup

--- app/src/main/java/com/example/empowertakehomeapp/BeneficiaryJSONReader.kt
+++ app/src/main/java/com/example/empowertakehomeapp/BeneficiaryJSONReader.kt
@@ -3,6 +3,7 @@ package com.example.empowertakehomeapp
 import android.content.Context
 
 class BeneficiaryJSONReader {
+    // companion object to hold static method
     companion object {
         // Reads a json file, given context and filename
         fun read(context: Context, fileName: String): String {
